1ba1a8475c1c2f59171fd2e4ad842357ee3ae62f
oda <odaimoko@gmail.com>
2020-02-18 21:53:11 +0800

    Framework of indexing

--- src/arxiv_indexing.py
+++ src/arxiv_indexing.py
@@ -14,13 +14,13 @@ class PostingElement:
         Data structure for one element in posting list
     """
 
-    def __init__(self, doc_id):
+    def __init__(self, doc_id, author=False):
         self.doc_id = doc_id
-        self.is_author = False
+        self.author = author
         self.positions = []
         raise NotImplementedError
 
-    def add():
+    def add_pos(pos):
         pass
 
 
@@ -31,10 +31,17 @@ class PostingList:
 
     def __init__(self, term):
         self.term = term
+        # TODO: TO be decided whether we use a list or a binary tree
+        self.doc_ids = {}  # doc_id: idx in list
+        self.doc_list = []
         raise NotImplementedError
 
-    def add_doc(self, doc_id):
-        raise NotImplementedError
+    def get_doc_posting(self, article):
+        doc_id = article['id']
+        if doc_id not in self.doc_ids:
+            self.doc_list.append(PostingElement(doc_id))
+            self.doc_ids[doc_id] = len(self.doc_list)-1
+        return self.doc_list[self.doc_ids[doc_id]]
 
     def encode(self):
         """
@@ -62,121 +69,119 @@ class PostingList:
         """
 
 
-def get_term_key(term):
-    """
-        convert a term into key. Used to 1. find posting list in cache 2.
-    """
-    return term
+class BuildIndex:
+    def __init__(self, args):
+        self.cfg = get_config(args)
 
+    def get_term_key(self, term):
+        """
+            convert a term into key. Terms with the same key will be saved into the same file.
+            Used to find posting list group in cached_posting_list.
+            See Index Storage and Compression section in report.
+        """
+        return term
 
-def get_term_index_file(key, index_dir):
-    """
-        Find the index file associated with this key,
-    """
-    pass
-
-
-def get_posting_list(term):
-    """
-        Get a posting list using term as the key
-    """
-    global cached_posting_list
-    key = get_term_key(term)
-    if key in cached_posting_list:
-        # TODO: load
-        raise NotImplementedError
-    else:
-        # TODO load from disk using get_term_index_file, decode it with PostingList.decode()
-        #  and add to cache
-        # Remember to save to disk if some cached posting is discarded
+    def save_posting_list_group(self, pl_group):
         raise NotImplementedError
 
+    def get_term_index_file(self, key, index_dir):
+        """
+            Find the index file associated with this key.
+            A file contains many posting lists (each associated with one term)
+        """
+        # TODO load from disk 
+        #  decode  with PostingList.decode()
+        # and return group of pls
+        return pl_group
 
-def preprocessing(stemmer, content, stop_words):
-    cleaned_list = []
-    for i in content:
-        i = i.lower()
-        if i.isalpha() and i not in stop_words:
-            i = stemmer.stem(i)
-            cleaned_list.append(i)
-    return cleaned_list
-
-
-def Doc(id, term_freq):
-    return {id: term_freq}
-
-
-def process_one_article(article):
-    """
-        Process one article
-    """
-
-    # TODO: edit these
-    doc_id = article['id']
-    title = article['title']
-    categories = article['categories']
-    abstract = article['abstract']
-    authors = article['authors']
-
-    content = nltk.word_tokenize(title + abstract)
-    cleaned_words = preprocessing(content)
-    for word in list(set(cleaned_words)):
-        pl = get_posting_list(word)
-        ...
-        d = Doc(doc_id, [pos for pos, x in enumerate(
-            cleaned_words) if x == word])
-
-        if word in postings_lists:
-            postings_lists[word].append(d)
+    def get_posting_list(self, term):
+        """
+            Get a posting list using term as the key
+        """
+        global cached_posting_list
+        key = self.get_term_key(term)
+        if key in cached_posting_list:
+            # DONE: load
+            d = cached_posting_list[key]
+            posting_list = d[term]
         else:
-            postings_lists[word] = [d]
+            pl_group = self.get_term_index_file(key, self.cfg['INDEX_DIR'])
+            # TODO add to cache
+            # Remember to save **asynchronously** to disk if some cached posting is discarded
+            raise NotImplementedError
+        return posting_list
 
-    raise NotImplementedError
+    def preprocessing(self, stemmer, content, stop_words, stemmer):
+        cleaned_list = []
+        for i in content:
+            i = i.lower()
+            if i.isalpha() and i not in stop_words:
+                i = stemmer.stem(i)
+                cleaned_list.append(i)
+        return cleaned_list
 
+    def Doc(self, id, term_freq):
+        return {id: term_freq}
 
-def build_index(gz_file, index_dir):
-    """
-        To build index using All data
-    """
-    raise NotImplementedError
+    def process_one_article(self, article, stop_words, stemmer):
+        """
+            Process one article
+        """
 
+        raise NotImplementedError
+        doc_id = article['id']
+        title = article['title']
+        categories = article['categories']
+        abstract = article['abstract']
+        authors = article['authors']
 
-def update_index(gz_file, index_dir):
-    """
-        To update current index using new data. 
-        Expect: All doc id are new 
-    """
-    raise NotImplementedError
+        # TODO: combine title, author and content
+        content = nltk.word_tokenize(title + abstract)
+        cleaned_words = self.preprocessing(content, stop_words, stemmer)
+        for pos, word in enumerate(cleaned_words):
+            pl: PostingList = self.get_posting_list(word)
+            doc_posting: PostingElement = pl.get_doc_posting(article)
+            doc_posting.add_pos(pos)
+        # TODO: build index for category?
 
+    def build_index(self):
+        """
+            To build index using All data
+        """
 
-def build_index_main(args):
-    global cached_posting_list
-    cfg = get_config(args)
-    nltk.download('stopwords')
+        raise NotImplementedError
+        # Only need to download once, please check this
+        nltk.download('stopwords')
+        stop_words = set(stopwords.words('english'))
+        ps = PorterStemmer()
+        with gzip.open(gz_file, 'rt', encoding='utf-8') as fin:
+            for line in tqdm.tqdm(fin.readlines()):
+                article = json.loads(line)
+                self.process_one_article(article, stop_words, ps)
 
-    stop_words = set(stopwords.words('english'))
-    ps = PorterStemmer()
-    postings_lists = {}
+    def update_index(self, gz_file, index_dir):
+        """
+            To update current index using new data. 
+            Expect: All doc id are new 
+        """
+        raise NotImplementedError
 
-    gz_file = cfg['ALL_DATA']
-    index_dir = cfg['INDEX_DIR']
-    with gzip.open(gz_file, 'rt', encoding='utf-8') as fin:
-        for line in tqdm.tqdm(fin.readlines()):
-            article = json.loads(line)
-            process_one_article(article)
+    def build_index_main(self, args):
+        # DONE
+        global cached_posting_list
+        self.build_index()
+        # save all the rest in cache
+        for k, pl in cached_posting_list.items():
+            assert type(pl) == PostingList
+            self.save_posting_list_group(pl)
 
-    js = json.dumps(postings_lists)
-    file = open('indexdict.txt', 'w')
-    file.write(js)
-    file.close()
-
-
-def update_index_main(args):
-    pass
+    def update_index_main(self, args):
+        pass
 
 
 if __name__ == "__main__":
     global cached_posting_list
-    cached_posting_list = {}
+    cached_posting_list = {}  # dict of group of posting lists
     args = args_build_index()
-    build_index_main(args)
+    tool = BuildIndex(args)
+    tool.build_index_main()
