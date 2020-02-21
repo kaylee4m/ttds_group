e982326570f563136b25e0b9f1489619ae4da7f3
oda <odaimoko@gmail.com>
2020-02-21 19:14:31 +0800

    v byte

--- src/utils.py
+++ src/utils.py
@@ -53,7 +53,6 @@ def get_cat_fullname(cat):
         global cat_abbr_to_full
         lines = [line.split() for line in con.strip().split('\n')]
         cat_abbr_to_full = {abbr: full for abbr, full in lines}
-    global cat_abbr_to_full
     return cat_abbr_to_full[cat]
 
 
@@ -64,18 +63,22 @@ def get_average_word_count():
 
 
 def get_int_doc_id(doc_id: str):
-    """TODO Find integer from
-
-    Arguments:
-        doc_id {str} -- [description]
-    """
-    global cfg
+    global cfg, doc_id_2_doc_no
     if not "doc_id_2_doc_no" in globals():
-        global doc_id_2_doc_no
-        with open(cfg['CAT_ABBR_DICT'], 'r') as f:
+        with open(cfg['DOC_ID_2_DOC_NO'], 'r') as f:
             doc_id_2_doc_no = json.load(f)
-    global doc_id_2_doc_no
     return doc_id_2_doc_no[doc_id]
+
+
+def get_str_doc_id(doc_id: int)->str:
+    global cfg, doc_no_2_doc_id, doc_id_2_doc_no
+    if not "doc_no_2_doc_id" in globals():
+        if not 'doc_id_2_doc_no' in globals():  # read from disk
+            with open(cfg['DOC_ID_2_DOC_NO'], 'r') as f:
+                doc_id_2_doc_no = json.load(f)
+        doc_no_2_doc_id = {v: k for k, v in doc_id_2_doc_no.items()}
+
+    return doc_no_2_doc_id[doc_id]
 
 
 def get_doc_numbers():
@@ -100,3 +103,48 @@ def get_doc_word_count(doc_id):
         # read from disk
 
         raise NotImplementedError
+
+
+def v_byte_encode(n: int) -> bytearray:
+    """Apply variable byte length compression to a number
+
+    Arguments:
+        n {int} -- [description]
+    """
+    b = bytearray()
+    while True:
+        b.insert(0, n % 128)
+        if n < 128:
+            break
+        n = int(n/128)
+    b[-1] += 128
+    return b
+
+
+def v_byte_decode(bytestream: bytearray) -> int:
+    nums = []
+    n = 0
+    for i, b in enumerate(bytestream):
+        if b < 128:
+            n = 128*n+b
+        else:
+            n = 128*n+(b-128)
+            nums.append(n)
+            n = 0
+    return nums
+
+
+if __name__ == "__main__":
+    # Test
+    answer1 = [b'\x81',
+               b'\x86',
+               b'\xff',
+               b'\x01\x80',
+               b'\x01\x82',
+               b'\x01\x1c\xa0', ]
+    encode_seq = [1, 6, 127, 128, 130, 20000]
+    for i, n in enumerate(encode_seq):
+        print(v_byte_encode(n) == answer1[i], v_byte_encode(n), answer1[i])
+    # Test Decode
+    decoded = v_byte_decode(b''.join(answer1))
+    print(decoded == encode_seq, decoded, encode_seq)
